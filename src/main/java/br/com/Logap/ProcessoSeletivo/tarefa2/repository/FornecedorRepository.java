000319c672393fc972a596a9d904a77824c30daa
Mathews Dantas <94187504+MathewsDantas@users.noreply.github.com>
2022-08-31 10:41:38 -0300

    Correção de error em FornecedorRepository.

--- src/main/java/br/com/Logap/ProcessoSeletivo/tarefa2/repository/FornecedorRepository.java
+++ src/main/java/br/com/Logap/ProcessoSeletivo/tarefa2/repository/FornecedorRepository.java
@@ -10,13 +10,6 @@ public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer>
     @Query("SELECT f FROM Fornecedor f WHERE (f.id) = :id")
     Fornecedor searchByProviderId(@Param("id") int id);
 
-
-    @Query("""
-            SELECT DISTINCT f
-              FROM Fornecedor f
-              JOIN Produto p ON p.fornecedor.id = f.id
-             WHERE p.qtd_produto = 0
-             
-            """)
+    @Query(" SELECT DISTINCT f FROM Fornecedor f JOIN Produto p ON p.fornecedor.id = f.id WHERE p.qtd_produto = 0 ")
     Iterable<Fornecedor> getOutOfStockProviders();
 }
